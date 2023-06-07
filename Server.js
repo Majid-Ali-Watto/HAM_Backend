432224bfcad7288d57925751ee64205806b69ab7
Majid Ali <majidaliqau@gmail.com>
2023-06-08 02:31:05 +0500

    routes for complaints added

--- Server.js
+++ Server.js
@@ -5,10 +5,12 @@ import StudentRouter from "./routes/students.js";
 import HostelRouter from "./routes/hostelSuperVisor.js";
 import SecurityRouter from "./routes/securitySuperVisor.js";
 import MessRouter from "./routes/messSuperVisor.js";
+import ComplaintsRouter from "./routes/complaints.js";
 app.use("/students", StudentRouter);
 app.use("/hostelSupervisor", HostelRouter);
 app.use("/securitySupervosor", SecurityRouter);
 app.use("/messSupervisor", MessRouter);
+app.use("/complaints", ComplaintsRouter);
 
 app.post("/getExitEntry", async (req, res) => {
 	try {
@@ -35,16 +37,20 @@ app.get("/todayMenu/:id", async (req, res) => {
 
 app.post("/exitentry", async (req, res) => {
 	try {
-		const { rollno, datetime, sem, exen, cnic } = req.body;
-		const result = await pool.query('INSERT INTO "exitentry" (rollno,semno,datetime,status,cnic) VALUES ($1,$2,$3,$4,$5)', [
+		const { rollno, sem, exen, cnic } = req.body;
+		const today = new Date();
+		let dateTime = today.toLocaleString();
+		const result = await pool.query("INSERT INTO public.exitentry(status, datetime, cnic, rollno, semno) VALUES ($1, $2, $3, $4, $5)", [
+			exen,
+			dateTime,
+			cnic,
 			rollno,
 			sem,
-			datetime,
-			exen,
-			cnic,
 		]);
+		console.log(result);
 		res.json(result);
 	} catch (error) {
+		console.log(error.message);
 		res.json(error.message);
 	}
 });
@@ -52,53 +58,6 @@ app.post("/exitentry", async (req, res) => {
 app.get("/getMenu", async (req, res) => {
 	try {
 		const allUsers = await pool.query('select * from "menu"');
-		res.json(allUsers.rows);
-	} catch (error) {
-		res.json(error.message);
-	}
-});
-/////////////////////////////////////////////////
-///////////////// Complaints ////////////////////
-////////////////////////////////////////////////
-
-app.post("/postComplaints", async (req, res) => {
-	try {
-		const { title, body, id, user, status } = req.body;
-		const result = await pool.query('INSERT INTO "Complaints" (title,body,id,complainer,status) VALUES ($1,$2,$3,$4,$5)', [
-			title,
-			body,
-			id,
-			user,
-			status,
-		]);
-		res.json(result);
-	} catch (error) {
-		res.json(error.message);
-	}
-});
-app.patch("/updateCompStatus", async (req, res) => {
-	try {
-		let { status, compID } = req.body;
-		let r = await pool.query('UPDATE "Complaints" SET status = $1 WHERE id = $2', [status, compID]);
-		if (r.rowCount > 0) res.send("Status Updated Successfully");
-		else res.send("Status not updated Successfully");
-	} catch (error) {
-		res.send(error.message);
-	}
-});
-app.delete("/removeComp/:id", async (req, res) => {
-	try {
-		let id = req.params.id;
-		let r = await pool.query('DELETE FROM "Complaints" WHERE id = $1', [id]);
-		if (r.rowCount > 0) res.send("Complaint deleted");
-		else res.send("Complaint not deleted");
-	} catch (error) {
-		res.send(error.message);
-	}
-});
-app.get("/allComplaints", async (req, res) => {
-	try {
-		const allUsers = await pool.query('select * from "Complaints"');
 		res.json(allUsers.rows);
 	} catch (error) {
 		res.json(error.message);
---
+++ routes/complaints.js
@@ -0,0 +1,57 @@
+/** @format */
+
+import { pool } from "../connection.js";
+import { Router } from "express";
+const ComplaintsRouter = Router();
+import NodeCache from "node-cache";
+const hamCache = new NodeCache();
+
+/////////////////////////////////////////////////
+///////////////// Complaints ////////////////////
+////////////////////////////////////////////////
+
+ComplaintsRouter.post("/postComplaints", async (req, res) => {
+	try {
+		const { title, body, id, user, status } = req.body;
+		const result = await pool.query('INSERT INTO "Complaints" (title,body,id,complainer,status) VALUES ($1,$2,$3,$4,$5)', [
+			title,
+			body,
+			id,
+			user,
+			status,
+		]);
+		res.json(result);
+	} catch (error) {
+		res.json(error.message);
+	}
+});
+ComplaintsRouter.patch("/updateCompStatus", async (req, res) => {
+	try {
+		let { status, compID } = req.body;
+		let r = await pool.query('UPDATE "Complaints" SET status = $1 WHERE id = $2', [status, compID]);
+		if (r.rowCount > 0) res.send("Status Updated Successfully");
+		else res.send("Status not updated Successfully");
+	} catch (error) {
+		res.send(error.message);
+	}
+});
+ComplaintsRouter.delete("/removeComp/:id", async (req, res) => {
+	try {
+		let id = req.params.id;
+		let r = await pool.query('DELETE FROM "Complaints" WHERE id = $1', [id]);
+		if (r.rowCount > 0) res.send("Complaint deleted");
+		else res.send("Complaint not deleted");
+	} catch (error) {
+		res.send(error.message);
+	}
+});
+ComplaintsRouter.get("/allComplaints", async (req, res) => {
+	try {
+		const allUsers = await pool.query('select * from "Complaints"');
+		res.json(allUsers.rows);
+	} catch (error) {
+		res.json(error.message);
+	}
+});
+
+export default ComplaintsRouter;
